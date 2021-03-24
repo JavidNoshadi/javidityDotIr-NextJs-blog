36b8ae6ff7e19424fa4a2ffbe6b60ddd0c1d34a7
javidnoshadi <javid.king63@gmail.com>
2021-03-24 23:06:29 +0330

    jlj

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   return <div><h1>hello wrld</h1></div>
 }
 
 export default MyApp
